411cd4cfff9714d8e7efac879199057652ef1ecd
Willian73 <will73tur@gmail.com>
2023-11-13 14:58:51 -0300

    Adding migration to insert data in products

---
+++ src/main/resources/db/migration/V0004__insert-data-products.sql
@@ -0,0 +1,5 @@
+INSERT INTO products(name, detail, price, category_id) VALUES('Filé a Parmegiana', 'Acompanha porção de arroz e saladas', 120.0, 1);
+INSERT INTO products(name, detail, price, category_id) VALUES('Frango Grelhado', 'Acompanha farofa e salada', 100.0, 1);
+INSERT INTO products(name, detail, price, category_id) VALUES('Água Tônica', 'Água Tônica sem açucar', 120.0, 2);
+INSERT INTO products(name, detail, price, category_id) VALUES('Doce de Leite', 'Doce de leite em pedaços', 120.0, 3);
+INSERT INTO products(name, detail, price, category_id) VALUES('Saladas', 'Salada de Alface e Tomate', 120.0, 4);
