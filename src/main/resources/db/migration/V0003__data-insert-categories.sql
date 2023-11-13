1d45103955ab92c28471963fa57bc920c4e5a051
Willian73 <will73tur@gmail.com>
2023-11-13 14:58:25 -0300

    Adding migration to insert data in categories

---
+++ src/main/resources/db/migration/V0003__data-insert-categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories(name) VALUE('Comida');
+INSERT INTO categories(name) VALUE('Bebida');
+INSERT INTO categories(name) VALUE('Sobremesa');
+INSERT INTO categories(name) VALUE('Fitness');
