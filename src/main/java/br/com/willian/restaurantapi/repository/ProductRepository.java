bdbbe4c8370ed57e8b5d85865420234ce8adc458
Willian73 <will73tur@gmail.com>
2023-11-26 16:44:07 -0300

    Adding Repository to Product

--- src/main/java/br/com/willian/restaurantapi/repository/ProductRepository.java
+++ src/main/java/br/com/willian/restaurantapi/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package br.com.willian.restaurantapi.repository;
 
 import br.com.willian.restaurantapi.model.entitys.Product;
+import br.com.willian.restaurantapi.model.projections.ProductProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query(value = "SELECT p.name, p.detail, p.price, c.name as category FROM products p INNER JOIN categories c ON p.category_id=c.id", nativeQuery = true)
+    List<ProductProjection> findAllProduct();
 }
